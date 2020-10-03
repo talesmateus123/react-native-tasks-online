d099681593abb07f1b2cce67154f520b9e7c110b
Tales Ferreira <talesmateus1999@hotmail.com>
2020-10-03 15:44:04 -0400

    Implemented create account template in Auth

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { 
     ImageBackground, Text, StyleSheet, 
-    View, TextInput, TouchableOpacity 
+    View, TextInput, TouchableOpacity,
+    Alert
 } from 'react-native'
 
 import backgroundImage from '../../assets/imgs/login.jpg'
@@ -11,7 +12,19 @@ export default class Auth extends Component {
     
     state = {
         email: '',
-        password: ''
+        password: '',
+        name: '',
+        confirmPassword: '',
+        stageNew: false
+    }
+
+    signInOrSignUp = () => {
+        if (this.state.stageNew) {
+            Alert.alert('Sucesso', 'Criar conta')
+        }
+        else {
+            Alert.alert('Sucesso', 'Logar')
+        }
     }
 
     render() {
@@ -19,14 +32,49 @@ export default class Auth extends Component {
             <ImageBackground source={backgroundImage} style={styles.background}>
                 <Text style={styles.title}>Tasks</Text>
                 <View style={styles.formContainer}>
-                    <TextInput placeholder="E-mail" value={this.state.email} style={styles.input} onChangeText={email => this.setState({ email })} />
-                    <TextInput placeholder="Senha" value={this.state.email} style={styles.input} onChangeText={password => this.setState({ password })} />
-                    <TouchableOpacity style={styles.button}>
+                    <Text style={styles.subTitle}>{this.state.stageNew ? 'Crie sua conta': 'Informe seus dados'}</Text>
+                    {this.state.stageNew &&
+                        <TextInput 
+                            placeholder="Nome" 
+                            value={this.state.name} 
+                            style={styles.input} 
+                            onChangeText={name => this.setState({ name })} />
+                    }
+                    <TextInput 
+                        placeholder="E-mail" 
+                        value={this.state.email} 
+                        style={styles.input} 
+                        onChangeText={email => this.setState({ email })} />
+                    <TextInput 
+                        placeholder="Senha"
+                        value={this.state.password} 
+                        style={styles.input} 
+                        onChangeText={password => this.setState({ password })}
+                        secureTextEntry={true} />
+                    
+                    {this.state.stageNew &&
+                        <TextInput 
+                            placeholder="Confirmar senha"
+                            value={this.state.confirmPassword} 
+                            style={styles.input} 
+                            onChangeText={confirmPassword => this.setState({ confirmPassword })}
+                            secureTextEntry={true} />
+                    }
+                    <TouchableOpacity style={styles.button} onPress={this.signInOrSignUp}>
                         <View>
-                            <Text style={styles.buttonText}>Entrar</Text>
+                            <Text style={styles.buttonText}>
+                                {this.state.stageNew ? 'Registrar': 'Entrar'}
+                            </Text>
                         </View>
                     </TouchableOpacity>
                 </View>
+                <TouchableOpacity onPress={() => this.setState({stageNew: !this.state.stageNew})}>
+                    <View>
+                        <Text style={styles.buttonText}>
+                        {this.state.stageNew ? 'Já possui conta?': 'Ainda não possui conta?'}
+                        </Text>
+                    </View>
+                </TouchableOpacity>
             </ImageBackground>
         )
     }
@@ -43,6 +91,13 @@ const styles = StyleSheet.create({
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.secundary,
         fontSize: 70,
+        marginBottom: 10
+    },
+    subTitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20,
+        textAlign: 'center',
         marginBottom: 10
     },
     formContainer: {
