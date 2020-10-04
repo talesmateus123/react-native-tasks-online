218cb66fcbe56b2041627762c998c5e772fbb6cd
Tales Ferreira <talesmateus1999@hotmail.com>
2020-10-03 22:37:30 -0400

    Signup, Signin and validation are implemented

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -4,31 +4,81 @@ import {
     View, TouchableOpacity, Alert
 } from 'react-native'
 
+import axios from 'axios'
+
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
 
 import AuthInput from '../components/AuthInput'
 
+import { server, showError, showSuccess } from '../common'
+
+const initialState = {
+    email: 'talesmateus1999@hotmail.com',
+    password: '21142307',
+    name: '',
+    confirmPassword: '',
+    stageNew: false
+}
+
 export default class Auth extends Component {
     
     state = {
-        email: '',
-        password: '',
-        name: '',
-        confirmPassword: '',
-        stageNew: false
+        ...initialState
     }
 
     signInOrSignUp = () => {
         if (this.state.stageNew) {
-            Alert.alert('Sucesso', 'Criar conta')
+            this.signup()
         }
         else {
-            Alert.alert('Sucesso', 'Logar')
+            this.signin()
         }
     }
 
+    signup = async () => {
+        try {
+            await axios.post(`${server}/signup`, {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword
+            })
+            showSuccess('Usuário cadastrado')
+            this.setState({...initialState})
+        }
+        catch(e) {
+            showError(e)
+        }
+    }
+
+    signin = async () => {
+        try {
+            const res = await axios.post(`${server}/signin`, {
+                email: this.state.email,
+                password: this.state.password,
+            })
+
+            axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
+            this.props.navigation.navigate('Home')
+        }
+        catch(e) {
+            showError(e)
+        }
+    }
+
+
     render() {
+        const validations = []
+        validations.push(this.state.email && this.state.email.includes('@'))
+        validations.push(this.state.password && this.state.password.length >= 6)
+        if (this.state.stageNew) {
+            validations.push(this.state.name && this.name.trim().length >= 3)
+            validations.push(this.password === this.state.confirmPassword)
+        }
+
+        const validForm = validations.reduce((t, a) => t && a)
+
         return (
             <ImageBackground source={backgroundImage} style={styles.background}>
                 <Text style={styles.title}>Tasks</Text>
@@ -65,15 +115,20 @@ export default class Auth extends Component {
                             onChangeText={confirmPassword => this.setState({ confirmPassword })}
                             secureTextEntry={true} />
                     }
-                    <TouchableOpacity style={styles.button} onPress={this.signInOrSignUp}>
-                        <View>
+                    <TouchableOpacity 
+                        onPress={this.signInOrSignUp}
+                        disabled={!validForm} >
+                        <View 
+                            style={[styles.button, validForm ? {} : {backgroundColor: '#AAA'} ]} >
                             <Text style={styles.buttonText}>
                                 {this.state.stageNew ? 'Registrar': 'Entrar'}
                             </Text>
                         </View>
                     </TouchableOpacity>
                 </View>
-                <TouchableOpacity onPress={() => this.setState({stageNew: !this.state.stageNew})}>
+                <TouchableOpacity 
+                    onPress={() => this.setState({stageNew: !this.state.stageNew})}
+                    >
                     <View>
                         <Text style={styles.buttonText}>
                         {this.state.stageNew ? 'Já possui conta?': 'Ainda não possui conta?'}
