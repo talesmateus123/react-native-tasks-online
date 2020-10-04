a08e3f9b3de56e34c89f2b1cf1c80fa1c2cf9f77
Tales Ferreira <talesmateus1999@hotmail.com>
2020-10-04 00:45:46 -0400

    Implemented logout on Menu componente, and added dynamic images in according to daysAhead property

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,26 +1,44 @@
 import React from 'react'
-import { ScrollView, View, Text, StyleSheet, Platform } from 'react-native'
+import { Platform, ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { DrawerItems } from 'react-navigation-drawer'
 import { Gravatar } from 'react-native-gravatar'
 import commonStyles from '../commonStyles'
 
-export default props => (
-    <ScrollView>
-        <View style={styles.header}>
-            <Text style={styles.title}>Tasks</Text>
-            <Gravatar style={styles.avatar}
-                options={{
-                    email: props.navigation.getParam('email'),
-                    secure: true,
-                }} />
-            <View style={styles.userInfo}>
-                <Text style={styles.name}>{props.navigation.getParam('name')}</Text>
-                <Text style={styles.email}>{props.navigation.getParam('email')}</Text>
+import axios from 'axios'
+import AsyncStorage from '@react-native-community/async-storage'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+export default props => {
+
+    const logout = () => {
+        delete axios.defaults.headers.common['Authorization']
+        AsyncStorage.removeItem('userData')
+        props.navigation.navigate('AuthOrApp')
+    }
+
+    return (
+        <ScrollView>
+            <View style={styles.header}>
+                <Text style={styles.title}>Tasks</Text>
+                <Gravatar style={styles.avatar}
+                    options={{
+                        email: props.navigation.getParam('email'),
+                        secure: true,
+                    }} />
+                <View style={styles.userInfo}>
+                    <Text style={styles.name}>{props.navigation.getParam('name')}</Text>
+                    <Text style={styles.email}>{props.navigation.getParam('email')}</Text>
+                </View>
+                <TouchableOpacity onPress={logout}>
+                    <View style={styles.logoutIcon}>
+                        <Icon name='sign-out' size={30} color='#800' />
+                    </View>
+                </TouchableOpacity>
             </View>
-        </View>
-        <DrawerItems {...props} />
-    </ScrollView>
-)
+            <DrawerItems {...props} />
+        </ScrollView>
+    )
+}
 
 const styles = StyleSheet.create({
     header: {
@@ -39,6 +57,8 @@ const styles = StyleSheet.create({
         height: 60,
         borderWidth: 3,
         borderRadius: 30,
+        marginLeft: 10,
+        marginBottom: 5,
     },
     userInfo: {
         marginLeft: 10,
@@ -54,5 +74,9 @@ const styles = StyleSheet.create({
         fontSize: 15,
         color: commonStyles.colors.subText,
         marginBottom: 10,
+    },
+    logoutIcon: {
+        marginLeft: 10,
+        marginBottom: 10
     }
 })
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -8,6 +8,9 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 
 import commonStyles from '../commonStyles'
 import todayImage from '../../assets/imgs/today.jpg'
+import tomorrowImage from '../../assets/imgs/tomorrow.jpg'
+import weekImage from '../../assets/imgs/week.jpg'
+import monthImage from '../../assets/imgs/month.jpg'
 
 import moment from 'moment'
 import 'moment/locale/pt-br'
@@ -108,13 +111,36 @@ export default class TaskList extends Component {
 
     render() {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
+
+        let styleColor = null
+        let image = null
+
+        switch(this.props.daysAhead) {
+            case 0:
+                styleColor = commonStyles.colors.today
+                image = todayImage
+                break
+            case 1:
+                styleColor = commonStyles.colors.tomorrow
+                image = tomorrowImage
+                break
+            case 7:
+                styleColor = commonStyles.colors.week
+                image = weekImage
+                break
+            default:
+                styleColor = commonStyles.colors.month
+                image = monthImage
+                break
+        }
+        
         return (
             <View style={styles.container}>
                 <AddTask 
                     isVisible={this.state.showAddTask}
                     onCancel={() => this.setState({ showAddTask: false })}
                     onSave={this.addTask} />
-                <ImageBackground source={todayImage} style={styles.background}>
+                <ImageBackground source={image} style={styles.background}>
                     <View style={styles.iconBar}>
                         <TouchableOpacity onPress={() => this.props.navigation.openDrawer()}>
                             <Icon 
