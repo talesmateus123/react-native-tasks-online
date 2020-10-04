de3e2c8e2bf409834c1ebcc98c6b57d4032e33a4
Tales Ferreira <talesmateus1999@hotmail.com>
2020-10-04 00:53:24 -0400

    Implemented some default messages in showError method

--- src/common.js
+++ src/common.js
@@ -3,7 +3,12 @@ import { Alert, Platform } from 'react-native'
 const server = Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000'
 
 function showError(err) {
-    Alert.alert('Ops! Ocorreu um problema', `Mensagem: ${err}`)
+    if (err.response && err.response.data) {
+        Alert.alert('Ops! Ocorreu um problema', `Mensagem: ${err.response.data}`)
+    }
+    else {
+        Alert.alert('Ops! Ocorreu um problema', `Mensagem: ${err.response.status}`)
+    }
 }
 
 function showSuccess(msg) {
