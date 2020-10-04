61d4cb1eb27aa71825983079c70c7f82d3a8cc58
Tales Ferreira <talesmateus1999@hotmail.com>
2020-10-04 00:03:21 -0400

    Gravadar implemented on Menu app

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,9 +1,36 @@
 import React from 'react'
-import { ScrollView } from 'react-native'
+import { ScrollView, View, Text, StyleSheet, Platform } from 'react-native'
 import { DrawerItems } from 'react-navigation-drawer'
+import { Gravatar } from 'react-native-gravatar'
 
 export default props => (
     <ScrollView>
+        <View style={styles.header}>
+            <Gravatar style={styles.avatar}
+                options={{
+                    email: props.navigation.getParam('email'),
+                    secure: true,
+                }} />
+            <View style={styles.userInfo}>
+                <Text>{props.navigation.getParam('name')}</Text>
+                <Text>{props.navigation.getParam('email')}</Text>
+            </View>
+        </View>
         <DrawerItems {...props} />
     </ScrollView>
 )
+
+const styles = StyleSheet.create({
+    header: {
+        borderBottomWidth: 1,
+        borderColor: '#DDD'
+    },
+    avatar: {
+        width: 60,
+        height: 60,
+        borderWidth: 3,
+        borderRadius: 30,
+        marginTop: 10,
+        margin: Platform.OS === 'ios' ? 30 : 10
+    }
+})
